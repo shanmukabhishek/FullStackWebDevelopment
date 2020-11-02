7161bdc37b5559dd82c0d754e7c392389677008c
shanmukabhishek <34566803+shanmukabhishek@users.noreply.github.com>
2020-11-02 23:51:50 +0530

    Update 04-events.js

--- hw2/04-events.js
+++ hw2/04-events.js
@@ -1,1 +1,12 @@
 // Enter your code here
+// Enter your code here
+window.addEventListener('resize', Size);
+    const widthOutput = document.querySelector('#width');
+    const heightOutput = document.querySelector('#height');
+    
+    function Size() {
+      widthOutput.textContent = window.innerWidth;
+      heightOutput.textContent = window.innerHeight;
+    }
+    
+    window.onresize = reportWindowSize;
