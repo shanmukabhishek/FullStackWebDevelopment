84f6461d2a2bba55ba682910a5086eab78de1ed3
shanmukabhishek <34566803+shanmukabhishek@users.noreply.github.com>
2020-11-02 23:52:54 +0530

    Update 05-api.js

--- hw2/05-api.js
+++ hw2/05-api.js
@@ -1,3 +1,20 @@
 const url = 'https://restcountries.eu/rest/v2/all';
 
 // Enter your code here
+function getpage(url) {
+    fetch(url).then(response => response.json()).
+        then(data => displayNames(data));
+}
+
+function displayNames(data){
+    var list = document.getElementById('results');
+    var newEntry =  null;
+    for(country of data){
+        newEntry = document.createElement('li');
+        newEntry.innerHTML=country.name+" - "+country.population.toLocaleString();
+        list.appendChild(newEntry);
+
+    }
+}
+
+getpage(url);
