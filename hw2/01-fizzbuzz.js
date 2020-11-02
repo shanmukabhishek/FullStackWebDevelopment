f3ad0523fe9b97135178390d7ed699a833a5bba7
shanmukabhishek <34566803+shanmukabhishek@users.noreply.github.com>
2020-11-02 23:49:27 +0530

    Update 01-fizzbuzz.js

--- hw2/01-fizzbuzz.js
+++ hw2/01-fizzbuzz.js
@@ -1,1 +1,16 @@
 // Enter your code here
+function fizzbuzz(){
+var str = document.getElementById("results");
+for (var i=1; i<=100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      str.innerHTML += "<div>fizzbuzz</div>";
+    } else if (i % 3 == 0) {
+      str.innerHTML += "<div>fizz</div>";
+    } else if (i % 5 == 0) {
+      str.innerHTML += "<div>buzz</div>";
+    } else {
+      str.innerHTML += "<div>" + i + "</div>";
+    }
+}
+}
+fizzbuzz();
