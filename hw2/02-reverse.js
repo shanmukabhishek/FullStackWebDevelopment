2f6ff62cd2ce41080063b4079778ded5e6d027fa
shanmukabhishek <34566803+shanmukabhishek@users.noreply.github.com>
2020-11-03 01:39:56 +0530

    Update 02-reverse.js

--- hw2/02-reverse.js
+++ hw2/02-reverse.js
@@ -1,1 +1,14 @@
-// Enter your code here
+document.getElementById("btn").addEventListener("click", reverseNumber);
+
+
+function reverseNumber()  
+{  
+    let number = document.getElementById("z").value;
+    if(number.length<8 || number.length>8){
+        alert("invalid input");
+    }
+    else{
+    let numReverse = number.split("").reverse().join("");
+    document.getElementById("hie").innerHTML = numReverse; 
+    }  
+}
