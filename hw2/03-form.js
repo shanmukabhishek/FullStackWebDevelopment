5593f0a3b9a7edecda6da2d373bdb5f80feed878
shanmukabhishek <34566803+shanmukabhishek@users.noreply.github.com>
2020-11-02 23:50:49 +0530

    Update 03-form.js

--- hw2/03-form.js
+++ hw2/03-form.js
@@ -1,1 +1,16 @@
 // Enter your code here
+// Enter your code here
+function handleSubmit(){
+var name= document.getElementById("name").value;
+var email= document.getElementById("email").value;
+var comments= document.getElementById("comments").value;
+var newsletter= document.getElementById("check").checked;
+
+console.log("name : "+name);
+console.log("email :"+email);
+console.log("comments: "+comments);
+console.log("newsletter :"+newsletter);
+}
+handleSubmit();
+
+
