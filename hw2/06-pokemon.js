69534fdd0b0b59e875dc07c3c6dce610e898f1d9
shanmukabhishek <34566803+shanmukabhishek@users.noreply.github.com>
2020-11-02 23:53:40 +0530

    Update 06-pokemon.js

--- hw2/06-pokemon.js
+++ hw2/06-pokemon.js
@@ -1,3 +1,22 @@
 const url = 'https://pokeapi.co/api/v2/pokemon/';
 
 // Enter your code here
+function getpage(url) {
+    fetch(url).then(response => response.json()).
+        then(data => displayNames(data));
+}
+
+function displayNames(data){
+    var list = document.getElementById('results');
+    var newEntry =  null;
+    for( poke of data.results){
+        newEntry = document.createElement('span');
+        newEntry.innerHTML=poke.name.charAt(0).toUpperCase()+poke.name.substr(1)+"&nbsp- ";
+        list.appendChild(newEntry);
+
+    }
+      if(data.next)
+    getpage(data.next);
+}
+
+getpage(url);
